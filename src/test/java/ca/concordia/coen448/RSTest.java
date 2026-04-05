db98a25751607564082a5752744a08403037fddf
George Git <georgenashed234@gmail.com>
2026-04-05 18:14:31 -0400

    new tests follow-up on tests done by QA team

--- src/test/java/ca/concordia/coen448/RSTest.java
+++ src/test/java/ca/concordia/coen448/RSTest.java
@@ -78,6 +78,48 @@ public class RSTest {
     }
 
     @Test
+    void TestRS_SmallStepCountsExact() {
+        RobotSimulator RS = new RobotSimulator();
+
+        RS.handleLine("D");
+        RS.handleLine("M 1");
+        assert(RS.getRow()==1);
+        assert(RS.getCol()==0);
+        assert(RS.getFloor().getCell(0, 0)==1);
+        assert(RS.getFloor().getCell(1, 0)==1);
+        assert(RS.getFloor().getCell(2, 0)==0);
+
+        RS.handleLine("R");
+        RS.handleLine("M 2");
+        assert(RS.getRow()==1);
+        assert(RS.getCol()==2);
+        assert(RS.getFloor().getCell(1, 1)==1);
+        assert(RS.getFloor().getCell(1, 2)==1);
+        assert(RS.getFloor().getCell(1, 3)==0);
+    }
+
+    @Test
+    void TestRS_EdgeStopsAtBoundary() {
+        RobotSimulator RS = new RobotSimulator();
+
+        RS.handleLine("D");
+        RS.handleLine("M 21");
+        assert(RS.getRow()==19);
+        assert(RS.getCol()==0);
+        for (int r = 0; r < 20; r++) {
+            assert(RS.getFloor().getCell(r, 0)==1);
+        }
+
+        RS.handleLine("R");
+        RS.handleLine("M 21");
+        assert(RS.getRow()==19);
+        assert(RS.getCol()==19);
+        for (int c = 0; c < 20; c++) {
+            assert(RS.getFloor().getCell(19, c)==1);
+        }
+    }
+
+    @Test
     void TestRS_Edge(){
         RobotSimulator RS=new RobotSimulator();
         
@@ -183,6 +225,51 @@ public class RSTest {
         for(int c:cols){
             assert(RS.getFloor().getCell(4,c)==1);
         }
+    }
+
+    @Test
+    void TestRS_ExactMarkingPattern() {
+        RobotSimulator RS = new RobotSimulator();
+
+        RS.handleLine("D");
+        RS.handleLine("M 4");
+        RS.handleLine("R");
+        RS.handleLine("M 4");
+        RS.handleLine("R");
+        RS.handleLine("M 4");
+        RS.handleLine("R");
+        RS.handleLine("M 4");
+
+        int[][] exp = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1}
+        };
+
+        for (int r = 0; r < 5; r++) {
+            for (int c = 0; c < 5; c++) {
+                assertEquals(exp[r][c], RS.getFloor().getCell(r, c),
+                        String.format("Cell does not match at (%d,%d)", r, c));
+            }
+        }
+    }
+
+    @Test
+    void TestRS_PenUpDoesNotMarkNewPath() {
+        RobotSimulator RS = new RobotSimulator();
+
+        RS.handleLine("D");
+        RS.handleLine("M 2"); 
+        RS.handleLine("R");
+        RS.handleLine("M 2"); 
+        RS.handleLine("U");  
+        RS.handleLine("L");  
+        RS.handleLine("M 2"); 
+
+        assert(RS.getFloor().getCell(3, 2)==0);
+        assert(RS.getFloor().getCell(4, 2)==0);
     }
 
     @ParameterizedTest
